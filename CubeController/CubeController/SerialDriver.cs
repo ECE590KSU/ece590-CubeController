855aa4a565d4533b21666ec5d361467e535f0fa8
Kevin Kleine <kkleine4@gmail.com>
2015-04-21 18:19:07 -0500

    General code clean up

--- CubeController/CubeController/SerialDriver.cs
+++ CubeController/CubeController/SerialDriver.cs
@@ -9,62 +9,96 @@ namespace CubeController
 {
     class SerialDriver
     {
+        /// <summary>
+        /// Cube dimension.
+        /// </summary>
         private const int DIMENSION = 8;
 
         /// <summary>
-        /// Active serial port
+        /// Active serial port.
         /// </summary>
         private SerialPort _serialPort;
 
         /// <summary>
-        /// Included for testing purposes. We can decide how
-        /// these two classes communicate later.
+        /// Default constructor for SerialDriver.
         /// </summary>
-        private Cube _cube;
-
-        /// <summary>
-        /// Default constructor for SerialDriver
-        /// </summary>
-        public SerialDriver ()
+        public SerialDriver (Cube cube)
         {
-            _cube = new Cube ();
             _serialPort = new SerialPort ();
 
             // Explicitly set for clarity
+            //_serialPort.PortName = 
             _serialPort.BaudRate = 9600;
             _serialPort.Parity = Parity.None;
             _serialPort.DataBits = 8;
             _serialPort.StopBits = StopBits.One;
             _serialPort.Handshake = Handshake.None;
             _serialPort.ReadTimeout = 500;
-            _serialPort.WriteTimeout = 500;            
+            _serialPort.WriteTimeout = 500;
         }
 
-        public void WriteCube ()
+        /// <summary>
+        /// Writes a buffer to the ATMega via the configured serial port.
+        /// </summary>
+        /// <param name="cubeState">The voxel state you wish to send to the cube.</param>
+        public void WriteCube (bool[][][] cubeState)
         {
-            byte[] buffer = ConvertCubeStateToByteArray();
+            byte[] buffer = ConvertCubeStateToByteArray (cubeState);
             _serialPort.Write(buffer, 0, buffer.Length);
         }
 
-        private byte[] ConvertCubeStateToByteArray ()
+        /// <summary>
+        /// Closes the serial port that has been opened.
+        /// </summary>
+        public void ClosePort ()
         {
-            bool[][][] cubeState = _cube.GetCubeState ();
-            Stack<byte> cubeStateBuffer = new Stack<byte>();
-            byte row;
+            _serialPort.Close();
+        }
+
+        /// <summary>
+        /// Opens a serial port.
+        /// </summary>
+        public void OpenPort ()
+        {
+            _serialPort.Open();
+        }
+
+        /// <summary>
+        /// Configures two serial settings on the port.
+        /// </summary>
+        /// <param name="baudRate">The serial baud rate to be set.</param>
+        /// <param name="writeTimeout">The number of miliseconds before a time-out occurs when a write operation does not finish.</param>
+        public void ConfigurePort (int baudRate, int writeTimeout)
+        {
+            _serialPort.BaudRate = baudRate;
+            _serialPort.WriteTimeout = writeTimeout;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cubeState">The voxel state you wish to send to the cube.</param>
+        /// <returns>A buffer to be sent over serial comm</returns>
+        private byte[] ConvertCubeStateToByteArray (bool[][][] cubeState)
+        {
+            List<byte> cubeStateBuffer = new List<byte> ();
+
             for (int x = 0; x < DIMENSION; x++)
             {
                 for (int y = 0; y < DIMENSION; y++)
                 {
-                    row = 0x00;
+                    byte row = 0x00;
                     for (int z = 0; z < DIMENSION; z++)
                     {
                         if (cubeState[x][y][z])
-                            row |= (byte)(0x1 << z);
+                            row |= (byte) (0x1 << z);
                     }
 
-                    cubeStateBuffer.Push(row);
+                    cubeStateBuffer.Add (row);
+                    // 0xFF is an escape code, so it must be sent twice if is actually 
+                    // intended to turn 8 LEDs on
                     if (row == (byte)0xff)
-                        cubeStateBuffer.Push(row);
+                        cubeStateBuffer.Add (row);
                 }
             }
 
