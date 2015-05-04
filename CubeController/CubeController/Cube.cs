967312662cf7b545d37761b151be16514c2aa40a
tylervanover <yeti.jones@gmail.com>
2015-05-03 20:27:25 -0500

    RandomSparkle has been created. Needs completion and testing

--- CubeController/CubeController/Cube.cs
+++ CubeController/CubeController/Cube.cs
@@ -1688,6 +1688,38 @@ namespace CubeController
 			}
 		}
 
+        /// <summary>
+        /// Blink an increasing number of random voxels, and then
+        /// blink in a decreasing number back down to 1 again. 
+        /// </summary>
+        public void RandomSparkle()
+        {
+
+        }
+
+        /// <summary>
+        /// Set a given number of voxels at random positions.
+        /// </summary>
+        /// <param name="iterations">Number of animation frames.</param>
+        /// <param name="voxels">Number of voxels to set.</param>
+        /// <param name="delay">Animation delay between frame updates.</param>
+        private void RandomSparkleFlash(int iterations, int voxels, int delay)
+        {
+            for (int i = 0; i < iterations; ++i)
+            {
+                for (int v = 0; v <= voxels; ++v)
+                {
+                    // Set a random voxel based. 
+                    SetVoxel(_rgen.Next() % DIMENSION, 
+                        _rgen.Next() % DIMENSION, 
+                        _rgen.Next() % DIMENSION);
+
+                    DelayMS(delay);
+                    ClearEntireCube();
+                }
+            }
+        }
+
 #endregion
 
 	}
